9b04fac250d73b20ebdb660fe58ca9c797f24a25
Isabella Salles <bellssalles@gmail.com>
2020-08-25 09:21:48 -0300

    :tada: First commit

---
+++ lib/game.ex
@@ -0,0 +1,8 @@
+defmodule ExMon.Game do
+  use Agent
+
+  def start(computer, player) do
+    initial_value = %{computer: computer, player: player, turn: :player, status: :started}
+    Agent.start_link(fn -> initial_value end, name: __MODULE__)
+  end
+end
