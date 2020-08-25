35cd12460d341be85749c6c06f47b1cccc6131c6
Isabella Salles <bellssalles@gmail.com>
2020-08-25 13:03:42 -0300

    :sparkles: Add player and npc

--- lib/game.ex
+++ lib/game.ex
@@ -5,4 +5,8 @@ defmodule ExMon.Game do
     initial_value = %{computer: computer, player: player, turn: :player, status: :started}
     Agent.start_link(fn -> initial_value end, name: __MODULE__)
   end
+
+  def info do
+    Agent.get(__MODULE__, & &1)
+  end
 end
