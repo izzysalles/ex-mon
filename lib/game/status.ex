1f46d8758b4c3d767c0f09934b5d383a493e54af
Isabella Salles <bellssalles@gmail.com>
2020-08-28 17:37:47 -0300

    :sparkles: Add new status

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -5,4 +5,17 @@ defmodule ExMon.Game.Status do
     IO.inspect(Game.info())
     IO.puts("--------------------")
   end
+
+  def print_wrong_move_message(move) do
+    IO.puts("\nInvalid move: #{move}\n")
+  end
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\nThe player attacked the computer dealing #{damage} damage\n")
+  end
+
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("\nThe computer attacked the computer dealing #{damage} damage\n")
+  end
+
 end
