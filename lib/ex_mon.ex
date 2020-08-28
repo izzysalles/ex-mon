a93b90258de2983bb6db718808235b3ed882b8c3
Isabella Salles <bellssalles@gmail.com>
2020-08-28 17:39:02 -0300

    :sparkles: Add player movements

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,6 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
-  alias ExMon.Game.Status
+  alias ExMon.Game.{Actions, Status}
 
   @computer_name "Jessie"
   def create_player(name, move_rnd, move_avg, move_heal) do
@@ -13,5 +13,20 @@ defmodule ExMon do
     |> Game.start(player)
 
     Status.print_round_message()
+  end
+
+  def make_move(move) do
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+  end
+
+  defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
+
+  defp do_move({:ok, move}) do
+    case move do
+      :move_heal -> Actions.heal()
+      move -> Actions.attack(move)
+    end
   end
 end
